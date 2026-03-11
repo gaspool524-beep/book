862bc6e39b9517f198b7f6004d65194e71dda9ab
gaspool524-beep <gaspool524@gmail.com>
2026-03-11 14:56:35 +0700

    Update d.js

--- d.js
+++ d.js
@@ -6,12 +6,3 @@ document.getElementById('book').addEventListener('click', function(e) {
     e.style.pointerEvents = ""
   }, 2000)
 })
-
-// tambahan buat play musik
-let start = document.getElementById("start");
-let music = document.getElementById("music");
-
-start.addEventListener("click", function(){
-  music.play();
-  start.style.display = "none";
-});
